15121cb36deb90e015182eac2812a44eee86d899
dimmel82 <37932300+dimmel82@users.noreply.github.com>
2021-06-24 10:47:27 +0300

    Update for AB#6

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
 	private String secretValue;	
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World again") String name, Model model) {
 		model.addAttribute("name", name + ". See my secret value which is not very secret: " + secretValue);
 		return "greeting";
 	}
